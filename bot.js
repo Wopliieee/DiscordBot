38ffe1315e6204a7344ff82f1c1cbb390998bdcf
Kamil <37486209+Wopliieee@users.noreply.github.com>
2019-02-02 16:23:15 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,12 +6,16 @@ bot.on('message', (message) => {
     
     msg = message.content.toLowerCase();
     
-    if(msg.startsWith == "siema") {
+    if(msg.startsWith ("siema")) {
         message.channel.sendMessage('Elko na rejonie');
     }
     
-    if(msg.startsWith == "lutka") {
+    if(msg.startsWith ("lutka")) {
         message.channel.send("No i mamy Gracjanka :)", {files: ["13925220_1745458395705484_7856054610386525895_n.jpg"]});
+    }
+        
+    if(msg.startsWith ("napisz za mnie" + zdanie)) {
+        message.channel.send("Nie wiem po co ale... " + zdanie);
     }
     
 });
