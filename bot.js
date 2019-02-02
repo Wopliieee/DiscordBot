0eefce2f2a0d8c3c03e211326af7ae77b138492e
Kamil <37486209+Wopliieee@users.noreply.github.com>
2019-02-02 16:03:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,11 @@ msg = msg.content.toLowerCase();
 
 bot.on('message', (message) => {
     
-    if(message.content == 'Siema') {
+    if(msg.startsWith == ("siema")) {
         message.channel.sendMessage('Elko na rejonie');
     }
     
-    if(msg.startsWith == "lutka") {
+    if(msg.startsWith == ("lutka")) {
         message.channel.send("No i mamy Gracjanka :)", {files: ["13925220_1745458395705484_7856054610386525895_n.jpg"]});
     }
     
