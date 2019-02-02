244a7b384981ac228e21a45c1d303ea43d6e97f8
Kamil <37486209+Wopliieee@users.noreply.github.com>
2019-02-02 16:39:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-        
+       
 
 bot.on('message', (message) => {
     
@@ -14,8 +14,19 @@ bot.on('message', (message) => {
         message.channel.send("No i mamy Gracjanka :)", {files: ["13925220_1745458395705484_7856054610386525895_n.jpg"]});
     }
         
-    if(msg.startsWith ("napisz za mnie" + zdanie)) {
-        message.channel.send("Nie wiem po co ale... " + zdanie);
+    if(msg.startsWith ("napisz za mnie")) {
+        editedmessage = message.content.slice (6);
+            
+        client.msgs [message.author.username] = {
+                message: message.content
+        }
+        fs.writeFile ("./msgs.json", JSON.stringify (client.msgs, null, 4), err => {
+                if (err) throw err;
+        });
+        
+        let _message = client.msgs[message.author.username].message;
+        message.channel.send("Nie wiem po co ale... " + _message)
+            
     }
     
 });
