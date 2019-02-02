fe0490b213c2934b222a6cdb2dada88e077c64e0
Kamil <37486209+Wopliieee@users.noreply.github.com>
2019-02-02 03:29:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,11 @@ bot.on('message', (message) => {
     if(message.content == 'Siema') {
         message.channel.sendMessage('Elko na rejonie');
     }
+    
+    if(message.content.startsWith ("Lutka")) {
+        message.channel.send("No i mamy Gracjanka :)", {file: ["./13925220_1745458395705484_7856054610386525895_n.jpg"]});
+    }
+    
 });
 
 // THIS  MUST  BE  THIS  WAY
