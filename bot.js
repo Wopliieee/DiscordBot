e0e9b1ce3066dcced4224213eb583532337622f7
Kamil <37486209+Wopliieee@users.noreply.github.com>
2019-02-02 16:58:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,11 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 
+bot.on("ready", (message) => {
+        message.channel.sendMessage('Obecny!');
+});
+
+
 const fs = require("fs");
 bot.msgs = require ("./msgs.json");
 
@@ -10,9 +15,14 @@ bot.on('message', (message) => {
     
     msg = message.content.toLowerCase();
     
-    if(msg.startsWith ("siema")) {
-        message.channel.sendMessage('Elko na rejonie');
+    if(msg.startsWith(prefix + "siema")) {
+        message.reply ('Siemanko!');
+        
+        message.channel.send ("message: " + message);
+        message.channel.send ("message sender: " + message.author.username);
+        message.channel.send ("message sender ID: " + message.author.id);
     }
+
     
     if(msg.startsWith ("lutka")) {
         message.channel.send("No i mamy Gracjanka :)", {files: ["13925220_1745458395705484_7856054610386525895_n.jpg"]});
