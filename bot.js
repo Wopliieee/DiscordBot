05f0dbd5811cf7b611b9424897c2f6d872a983cc
Kamil <37486209+Wopliieee@users.noreply.github.com>
2019-02-04 15:32:33 +0100

    dodane osoby :)

--- bot.js
+++ bot.js
@@ -16,12 +16,46 @@ bot.on('message', (message) => {
         message.channel.send ('A tak wyglądam :) https://github.com/Wopliieee/DiscordBot/blob/master/bot.js');
     }
     
+    
+    
+    
     if(msg.startsWith ("lutka")) {
-        message.channel.send("Po nazwisku to po pysku! Ale jak coś tak wygląda Gracjanek :)", {files: ["13925220_1745458395705484_7856054610386525895_n.jpg"]});
+        message.channel.send("Po nazwisku to po pysku! Ale jak coś tak wygląda Gracjanek :)", {files: ["gracjan.jpg"]});
     }
-    if(msg.startsWith ("debil")) {
-        message.channel.send("Po nazwisku to po pysku! Ale jak coś tak wygląda Gracjanek :)", {files: ["13925220_1745458395705484_7856054610386525895_n.jpg"]});
+    if(msg.startsWith ("romanek")) {
+        message.channel.send("Bardzo miły chłopak i tak On wygląda :)", {files: ["Roman.PNG"]});
     }
+    if(msg.startsWith ("fabianek")) {
+        message.channel.send("Fabian zawsze chętnie z tobą porozmawia :)", {files: ["fabian.jpg"]});
+    }
+    if(msg.startsWith ("julcia")) {
+        message.channel.send("Jedyna kobita na naszym Discordzie! Miła i ładna. :)", {files: ["julcia.PNG"]});
+    }
+    if(msg.startsWith ("kamilek")) {
+        message.channel.send("Kozak i chociaż nikogo nie zna to kogoś tam sobie pozna. :)", {files: ["kamilek.PNG"]});
+    }
+    if(msg.startsWith ("konradzik")) {
+        message.channel.send("Konradzik lubi wszystkich i bardzo przyjemny chłopak. :)", {files: ["konrad.PNG"]});
+    }
+    if(msg.startsWith ("kubuś")) {
+        message.channel.send("Kubuś może i powyzywa enemy i mówi że są rudymi kurwami, ale jest miły. :)", {files: ["kuba.PNG"]});
+    }
+    if(msg.startsWith ("mateuszek")) {
+        message.channel.send("Kto to?", {files: ["mateusz.png"]});
+    }
+    if(msg.startsWith ("oskarek")) {
+        message.channel.send("Stary i już na emeryturze ale jest spoko ziomkiem. :)", {files: ["oskar.PNG"]});
+    }
+    if(msg.startsWith ("szyszka")) {
+        message.channel.send("Czasem wbije na discorda. :)", {files: ["szyszka.PNG"]});
+    }
+    if(msg.startsWith ("wiktorek")) {
+        message.channel.send("Prezes naszego klubu. :)", {files: ["wiktor.PNG"]});
+    }
+    
+    
+    
+    
         
     if(msg.startsWith ("napisz")) {
         editedmessage = message.content.slice (7);
@@ -37,9 +71,9 @@ bot.on('message', (message) => {
         message.channel.send("Nie wiem po co ale... " + _message);
             
     }
-    
+
     if(msg.startsWith ("co umiesz")){
-        message.channel.send("Umiem dużo rzeczy ;) Np.: co umiesz, siema, jak wyglądasz, lutka, napisz[zdanie]");
+        message.channel.send("Umiem dużo rzeczy ;) Np.: co umiesz, siema, jak wyglądasz, napisz[zdanie], lutka, romanek, fabianek, julcia, kamilek, konradzik, kubuś, mateuszek, oskarek, szyszka, wiktorek");
     }
     
 });
